ddc4e72965a3511abdfd5eacb9adf011d8c022ae
MaxWong03 <madmaxmmajournal@gmail.com>
2019-07-17 16:27:31 -0700

    added swiper for pokemon slide

---
+++ script.js
@@ -0,0 +1,8 @@
+let mySwiper = new mySwiper('.swiper-container',{
+  direction: 'vertical',
+  loop: true,
+  navigation:{
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+}) 
